c81d2c8797c5a10fceac403b0cfc1fe0a3faa19f
Centaurus99 <1102569568@qq.com>
2021-09-05 16:56:25 +0800

    Fix bug in checker

--- checker.py
+++ checker.py
@@ -12,82 +12,49 @@ conn = sqlite3.connect('data/' + DB_NAME + '.sqlite3')
 conn.row_factory = sqlite3.Row
 cursor = conn.cursor()
 
-assert(cursor.execute(
-    'SELECT COUNT() FROM VIDEO WHERE bvid IS NULL;').fetchone()[0] == 0)
-assert(cursor.execute(
-    'SELECT COUNT() FROM VIDEO WHERE cid IS NULL;').fetchone()[0] == 0)
-assert(cursor.execute(
-    'SELECT COUNT() FROM VIDEO WHERE pic IS NULL;').fetchone()[0] == 0)
-assert(cursor.execute(
-    'SELECT COUNT() FROM VIDEO WHERE title IS NULL;').fetchone()[0] == 0)
-assert(cursor.execute(
-    'SELECT COUNT() FROM VIDEO WHERE desc IS NULL;').fetchone()[0] == 0)
-assert(cursor.execute(
-    'SELECT COUNT() FROM VIDEO WHERE keywords IS NULL;').fetchone()[0] == 0)
-assert(cursor.execute(
-    'SELECT COUNT() FROM VIDEO WHERE copyright IS NULL;').fetchone()[0] == 0)
-assert(cursor.execute(
-    'SELECT COUNT() FROM VIDEO WHERE duration IS NULL;').fetchone()[0] == 0)
-assert(cursor.execute(
-    'SELECT COUNT() FROM VIDEO WHERE videos IS NULL;').fetchone()[0] == 0)
-assert(cursor.execute(
-    'SELECT COUNT() FROM VIDEO WHERE pubdate IS NULL;').fetchone()[0] == 0)
-assert(cursor.execute(
-    'SELECT COUNT() FROM VIDEO WHERE view IS NULL;').fetchone()[0] == 0)
-assert(cursor.execute(
-    'SELECT COUNT() FROM VIDEO WHERE danmaku IS NULL;').fetchone()[0] == 0)
-assert(cursor.execute(
-    'SELECT COUNT() FROM VIDEO WHERE like IS NULL;').fetchone()[0] == 0)
-assert(cursor.execute(
-    'SELECT COUNT() FROM VIDEO WHERE coin IS NULL;').fetchone()[0] == 0)
-assert(cursor.execute(
-    'SELECT COUNT() FROM VIDEO WHERE favorite IS NULL;').fetchone()[0] == 0)
-assert(cursor.execute(
-    'SELECT COUNT() FROM VIDEO WHERE share IS NULL;').fetchone()[0] == 0)
-assert(cursor.execute(
-    'SELECT COUNT() FROM VIDEO WHERE reply IS NULL;').fetchone()[0] == 0)
-assert(cursor.execute(
-    'SELECT COUNT() FROM VIDEO WHERE owner IS NULL;').fetchone()[0] == 0)
-assert(cursor.execute(
-    'SELECT COUNT() FROM VIDEO WHERE localpic IS NULL;').fetchone()[0] == 0)
-assert(cursor.execute(
-    'SELECT COUNT() FROM VIDEO WHERE spider IS NULL;').fetchone()[0] == 0)
+assert(cursor.execute('SELECT COUNT() FROM VIDEO WHERE bvid IS NULL;').fetchone()[0] == 0)
+assert(cursor.execute('SELECT COUNT() FROM VIDEO WHERE cid IS NULL;').fetchone()[0] == 0)
+assert(cursor.execute('SELECT COUNT() FROM VIDEO WHERE pic IS NULL;').fetchone()[0] == 0)
+assert(cursor.execute('SELECT COUNT() FROM VIDEO WHERE title IS NULL;').fetchone()[0] == 0)
+assert(cursor.execute('SELECT COUNT() FROM VIDEO WHERE desc IS NULL;').fetchone()[0] == 0)
+assert(cursor.execute('SELECT COUNT() FROM VIDEO WHERE keywords IS NULL;').fetchone()[0] == 0)
+assert(cursor.execute('SELECT COUNT() FROM VIDEO WHERE copyright IS NULL;').fetchone()[0] == 0)
+assert(cursor.execute('SELECT COUNT() FROM VIDEO WHERE duration IS NULL;').fetchone()[0] == 0)
+assert(cursor.execute('SELECT COUNT() FROM VIDEO WHERE videos IS NULL;').fetchone()[0] == 0)
+assert(cursor.execute('SELECT COUNT() FROM VIDEO WHERE pubdate IS NULL;').fetchone()[0] == 0)
+assert(cursor.execute('SELECT COUNT() FROM VIDEO WHERE view IS NULL;').fetchone()[0] == 0)
+assert(cursor.execute('SELECT COUNT() FROM VIDEO WHERE danmaku IS NULL;').fetchone()[0] == 0)
+assert(cursor.execute('SELECT COUNT() FROM VIDEO WHERE like IS NULL;').fetchone()[0] == 0)
+assert(cursor.execute('SELECT COUNT() FROM VIDEO WHERE coin IS NULL;').fetchone()[0] == 0)
+assert(cursor.execute('SELECT COUNT() FROM VIDEO WHERE favorite IS NULL;').fetchone()[0] == 0)
+assert(cursor.execute('SELECT COUNT() FROM VIDEO WHERE share IS NULL;').fetchone()[0] == 0)
+assert(cursor.execute('SELECT COUNT() FROM VIDEO WHERE reply IS NULL;').fetchone()[0] == 0)
+assert(cursor.execute('SELECT COUNT() FROM VIDEO WHERE owner IS NULL;').fetchone()[0] == 0)
+assert(cursor.execute('SELECT COUNT() FROM VIDEO WHERE localpic IS NULL;').fetchone()[0] == 0)
+assert(cursor.execute('SELECT COUNT() FROM VIDEO WHERE spider IS NULL;').fetchone()[0] == 0)
 
-assert(cursor.execute(
-    'SELECT COUNT() FROM USER WHERE name IS NULL;').fetchone()[0] == 0)
-assert(cursor.execute(
-    'SELECT COUNT() FROM USER WHERE sex IS NULL;').fetchone()[0] == 0)
-assert(cursor.execute(
-    'SELECT COUNT() FROM USER WHERE face IS NULL;').fetchone()[0] == 0)
-assert(cursor.execute(
-    'SELECT COUNT() FROM USER WHERE sign IS NULL;').fetchone()[0] == 0)
-assert(cursor.execute(
-    'SELECT COUNT() FROM USER WHERE level IS NULL;').fetchone()[0] == 0)
-assert(cursor.execute(
-    'SELECT COUNT() FROM USER WHERE attention IS NULL;').fetchone()[0] == 0)
-assert(cursor.execute(
-    'SELECT COUNT() FROM USER WHERE fans IS NULL;').fetchone()[0] == 0)
-assert(cursor.execute(
-    'SELECT COUNT() FROM USER WHERE localpic IS NULL;').fetchone()[0] == 0)
-assert(cursor.execute(
-    'SELECT COUNT() FROM USER WHERE spider IS NULL;').fetchone()[0] == 0)
+assert(cursor.execute('SELECT COUNT() FROM USER WHERE name IS NULL;').fetchone()[0] == 0)
+assert(cursor.execute('SELECT COUNT() FROM USER WHERE sex IS NULL;').fetchone()[0] == 0)
+assert(cursor.execute('SELECT COUNT() FROM USER WHERE face IS NULL;').fetchone()[0] == 0)
+assert(cursor.execute('SELECT COUNT() FROM USER WHERE sign IS NULL;').fetchone()[0] == 0)
+assert(cursor.execute('SELECT COUNT() FROM USER WHERE level IS NULL;').fetchone()[0] == 0)
+assert(cursor.execute('SELECT COUNT() FROM USER WHERE attention IS NULL;').fetchone()[0] == 0)
+assert(cursor.execute('SELECT COUNT() FROM USER WHERE fans IS NULL;').fetchone()[0] == 0)
+assert(cursor.execute('SELECT COUNT() FROM USER WHERE localpic IS NULL;').fetchone()[0] == 0)
+assert(cursor.execute('SELECT COUNT() FROM USER WHERE spider IS NULL;').fetchone()[0] == 0)
 
-assert(cursor.execute(
-    'SELECT COUNT() FROM COMMENT WHERE data IS NULL;').fetchone()[0] == 0)
-assert(cursor.execute(
-    'SELECT COUNT() FROM COMMENT WHERE spider IS NULL;').fetchone()[0] == 0)
+assert(cursor.execute('SELECT COUNT() FROM COMMENT WHERE data IS NULL;').fetchone()[0] == 0)
+assert(cursor.execute('SELECT COUNT() FROM COMMENT WHERE spider IS NULL;').fetchone()[0] == 0)
 
 print('NULL check pass.')
 
 
-assert(cursor.execute(
-    'SELECT COUNT() from VIDEO WHERE VIDEO.owner NOT IN (SELECT mid FROM USER)').fetchone()[0] == 0)
+assert(cursor.execute('SELECT COUNT() from VIDEO WHERE VIDEO.owner NOT IN (SELECT mid FROM USER)').fetchone()[0] == 0)
 
 print('USER existence check pass.')
 
 
-assert(cursor.execute(
-    'SELECT COUNT() from VIDEO WHERE VIDEO.aid NOT IN (SELECT oid FROM COMMENT)').fetchone()[0] == 0)
+assert(cursor.execute('SELECT COUNT() from VIDEO WHERE VIDEO.aid NOT IN (SELECT oid FROM COMMENT)').fetchone()[0] == 0)
 
 print('COMMENT existence check pass.')
 
@@ -103,7 +70,7 @@ print('VIDEO pic check pass.')
 cursor = conn.execute('SELECT * FROM USER WHERE localpic = 1')
 for row in tqdm(cursor):
     Image.open(
-        'data/user_face/{}.{}'.format(row['mid'], row['pic'].split('.')[-1])).verify()
+        'data/user_face/{}.{}'.format(row['mid'], row['face'].split('.')[-1])).verify()
 
 print('USER pic check pass.')
 
